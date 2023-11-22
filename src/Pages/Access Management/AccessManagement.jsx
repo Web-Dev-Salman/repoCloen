05d1070d237e68971c2593cc520860011b8e1baf
Taufeeq28 <sameerahmed6686@gmail.com>
2023-11-22 14:43:38 -0600

    trial commit

--- src/Pages/Access Management/AccessManagement.jsx
+++ src/Pages/Access Management/AccessManagement.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AccessManagement = () => {
     return (
         <div>
-            <h1>Access Management</h1>
+            <h1>Access Management </h1>
         </div>
     );
 };
